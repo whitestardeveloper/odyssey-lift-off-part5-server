bf986cfe52895d718a47e035fda22ff311046b38
Abdülkadir AKYILDIZ <abdulkadir.akyildiz@aurorabilisim.com>
2022-03-30 17:37:21 +0300

    asd

--- src/index.js
+++ src/index.js
@@ -2,23 +2,30 @@ const { ApolloServer } = require("apollo-server");
 const typeDefs = require("./schema");
 const resolvers = require("./resolvers");
 const TrackAPI = require("./datasources/track-api");
+const express = require('express');
+const cors = require('cors');
 
-const link = createHttpLink({
-  uri: '*',
-  credentials: 'same-origin'
-});
+const app = express();
+
+// CORS configuration
+const corsOptions = {
+  origin: true,
+  credentials: true
+}
 
 async function startApolloServer(typeDefs, resolvers) {
   const server = new ApolloServer({
     typeDefs,
     resolvers,
-    link,
+    cors: cors(corsOptions),
     dataSources: () => {
       return {
         trackAPI: new TrackAPI(),
       };
     },
   });
+
+  server.applyMiddleware({ app });
 
   const { url, port } = await server.listen({ port: precess.env.PORT || 4000 });
   console.log(`
