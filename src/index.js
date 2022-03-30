95ba3fbf5d7d8b5abbda393c7ba48f14d928a007
Abdülkadir AKYILDIZ <abdulkadir.akyildiz@aurorabilisim.com>
2022-03-30 17:25:55 +0300

    asd

--- src/index.js
+++ src/index.js
@@ -1,38 +1,24 @@
 const { ApolloServer } = require("apollo-server");
-const express = require('express');
 const typeDefs = require("./schema");
 const resolvers = require("./resolvers");
 const TrackAPI = require("./datasources/track-api");
+
+const link = createHttpLink({
+  uri: '*',
+  credentials: 'same-origin'
+});
 
 async function startApolloServer(typeDefs, resolvers) {
   const server = new ApolloServer({
     typeDefs,
     resolvers,
-    cors: {
-      origin: 'http://localhost:3000',
-      credentials: true
-    },
+    link,
     dataSources: () => {
       return {
         trackAPI: new TrackAPI(),
       };
     },
   });
-
-
-const app = express();
-
-var corsOptions = {
-  origin:'https://client-castsronauts.herokuapp.com/',
-  credentials: true // <-- REQUIRED backend setting
-};
-
-app.use(cors(corsOptions));
-server.applyMiddleware({
-  app,
-  path: '/',
-  cors: false, // disables the apollo-server-express cors to allow the cors middleware use
-})
 
   const { url, port } = await server.listen({ port: precess.env.PORT || 4000 });
   console.log(`
