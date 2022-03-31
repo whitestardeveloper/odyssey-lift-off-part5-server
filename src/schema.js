8d22fc838b5f315ae56561e5cf2c8207898f218b
Abdülkadir AKYILDIZ <abdulkadir.akyildiz@aurorabilisim.com>
2022-03-31 10:01:41 +0300

    deprecated length to durationInSeconds

--- src/schema.js
+++ src/schema.js
@@ -36,7 +36,8 @@ const typeDefs = gql`
     "The track's illustration to display in track card or track page detail"
     thumbnail: String
     "The track's approximate length to complete, in minutes"
-    length: Int
+    length: Int @deprecated(reason: "Use durationInSeconds")
+    durationInSeconds: Int
     "The number of modules this track contains"
     modulesCount: Int
     "The track's complete description, can be in markdown format"
@@ -62,7 +63,8 @@ const typeDefs = gql`
     "The module's title"
     title: String!
     "The module's length in minutes"
-    length: Int
+    length: Int @deprecated(reason: "Use durationInSeconds")
+    durationInSeconds: Int
     "The module's text-based description, can be in markdown format. In case of a video, it will be the enriched transcript"
     content: String
     "The module's video url, for video-based modules"
